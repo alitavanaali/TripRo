ced88e40eff39b11b15d2d3ff6ca4e85a9cab74f
alitavanaali <alitavanaali@gmail.com>
2017-05-31 19:49:21 +0430

    Eric Class Added!

---
+++ app/src/main/java/alitavana/com/tripro/activity/EricTest.java
@@ -0,0 +1,11 @@
+package alitavana.com.tripro.activity;
+
+/**
+ * Created by Ali Tavana on 31/05/2017.
+ */
+
+public class EricTest {
+    public EricTest() {
+        System.out.println("salam");
+    }
+}
